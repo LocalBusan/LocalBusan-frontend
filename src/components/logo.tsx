b10c329bd0ae59ccc64d9812671f38e99c8eb180
jihongeek <jihongeek@kakao.com>
2024-11-16 16:00:38 +0900

    Feat: 로고 클릭시 메인 페이지로 이동하게 하기

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,4 +1,9 @@
 import Image from "next/image";
+import Link from "next/link";
 export function Logo() {
-    return <Image priority={true} src="./logo.svg" height={85} width={219} alt="로컬부산 로고"/>;
+    return (
+        <Link href="/">
+            <Image priority={true} src="./logo.svg" height={85} width={219} alt="로컬부산 로고"/>
+        </Link>
+    );
 }
