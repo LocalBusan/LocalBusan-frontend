3b672826910659f4b13335e4748efe5baf3e39db
jihongeek <jihongeek@kakao.com>
2024-11-17 20:19:44 +0900

    Chore: logo 컴포넌트 경로 변경으로 인한 import 변경

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,10 +1,10 @@
 import { LoginForm } from "@/components/login-form"
-import {Logo} from "@/components/logo";
+import {Logo} from "@/components/ui/logo";
 export default function Page() {
   return (
       <div className="flex flex-col gap-y-10 h-screen w-full items-center justify-center px-4">
         <Logo/>
-        <LoginForm />
+        <LoginForm/>
       </div>
   )
 }
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,5 +1,5 @@
 import { SignupForm } from "@/components/signup-form"
-import {Logo} from "@/components/logo";
+import {Logo} from "@/components/ui/logo";
 export default function Page() {
   return (
     <div className="flex flex-col gap-y-10 h-screen w-full items-center justify-center px-4">
