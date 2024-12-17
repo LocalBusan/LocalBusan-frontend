c89f591bb131ace6f0c1f66f6848344f75f1e283
jihongeek <jihongeek@kakao.com>
2024-12-17 23:03:17 +0900

    Chore: 챗봇 Api를 위한 rewrite 작성

--- next.config.ts
+++ next.config.ts
@@ -8,9 +8,13 @@ const nextConfig: NextConfig = {
   async rewrites() {
     return [
       {
+        source: '/chatbot/:slug*',
+        destination : 'http://113.198.229.158:4444/generate/:slug*'
+      },
+      {
         source: '/api/:slug*',
         destination : 'http://3.34.225.212:8080/api/:slug*'
-      }
+      },
     ]
   }
 };
