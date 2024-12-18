d3459b66b058c4d0cf8263ab20f7ce38679d50f6
jihongeek <jihongeek@kakao.com>
2024-12-18 14:45:11 +0900

    Chore : 썸네일 관련 코드 수정

--- src/app/main/page.tsx
+++ src/app/main/page.tsx
@@ -148,7 +148,7 @@ export default function Home() {
         articleState.filter(selectCheck).map(({region,category,created_at,article_id, title ,subtitle, thumbnail_url} : any,index)=>{
           return(
           <Link key={index} href={`/article/${article_id}`} className="w-[300px] flex flex-col">
-              <AspectRatio ratio={16 / 9}>
+              <AspectRatio ratio={16 / 9} className="flex justify-center">
                   <img src={thumbnail_url} width="70%" alt="Image" className="rounded-md object-cover" onError={({currentTarget})=>{currentTarget.src = './alt_image.png'}}/>
               </AspectRatio>
               <div className="text-amber-800 font-bold">{category.category_name} in {region.region_name}</div>
